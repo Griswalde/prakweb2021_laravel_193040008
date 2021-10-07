edaa20b47399bca7e9c0dba8c47c459310976cdc
Griswalde <agunggriswald158@gmail.com>
2021-10-07 08:32:08 +0700

    #4 Blade Templating Engine

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home') ;
+    return view('home', [
+        "tittle" => "Home"
+    ]) ;
 });
 
 
 Route::get('/about', function () {
     return view('about', [
+        "tittle" => "About",
         "name" => "Baby Yoda",
         "email" => "agunggriswald158@gmail.com",
         "image" => "yoda.jpg"
@@ -27,6 +30,84 @@ Route::get('/about', function () {
 });
 
 
-Route::get('/blog', function () {
-    return view('posts') ;
+
+Route::get('/blog', function(){
+    $blog_posts = [
+        [
+            "tittle" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Baby Yoda",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Accusamus pariatur repellat modi, 
+            architecto maxime enim. Nam ex voluptates laborum iure, 
+            molestiae, ipsam inventore veniam atque eum numquam similique impedit vel quaerat reprehenderit commodi,
+             eligendi vitae sed distinctio. Eaque impedit incidunt nesciunt facilis sapiente. Debitis ipsum quo quis eius, 
+             voluptas quaerat?"
+        ],
+        [
+        "tittle" => "Judul Post kedua",
+        "slug" => "judul-post-kedua",
+        "author" => "Mando",
+        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Accusamus pariatur repellat modi, 
+        architecto maxime enim. Nam ex voluptates laborum iure, 
+        molestiae, ipsam inventore veniam atque eum numquam similique impedit vel quaerat reprehenderit commodi,
+         eligendi vitae sed distinctio. Eaque impedit incidunt nesciunt facilis sapiente. Debitis ipsum quo quis eius, 
+         voluptas quaerat?"
+        ],
+        ];
+        
+    
+    return view('posts', [
+        "tittle" => "Posts",
+        "posts" => $blog_posts
+    ]);
 });
+
+
+
+// halaman single
+
+Route::get('posts/{slug}', function($slug) {
+    $blog_posts = [
+        [
+            "tittle" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Baby Yoda",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Accusamus pariatur repellat modi, 
+            architecto maxime enim. Nam ex voluptates laborum iure, 
+            molestiae, ipsam inventore veniam atque eum numquam similique impedit vel quaerat reprehenderit commodi,
+             eligendi vitae sed distinctio. Eaque impedit incidunt nesciunt facilis sapiente. Debitis ipsum quo quis eius, 
+             voluptas quaerat?"
+        ],
+        [
+        "tittle" => "Judul Post kedua",
+        "slug" => "judul-post-kedua",
+        "author" => "Mando",
+        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Accusamus pariatur repellat modi, 
+        architecto maxime enim. Nam ex voluptates laborum iure, 
+        molestiae, ipsam inventore veniam atque eum numquam similique impedit vel quaerat reprehenderit commodi,
+         eligendi vitae sed distinctio. Eaque impedit incidunt nesciunt facilis sapiente. Debitis ipsum quo quis eius, 
+         voluptas quaerat?"
+        ],
+        ];
+
+
+        $new_post = [];
+        foreach($blog_posts as $post) {
+            if($post["slug"] === $slug) {
+                $new_post = $post;
+            }
+        }
+    
+    return view('post',[
+        "tittle" =>"Single Post",
+        "post" => $new_post
+    ]);
+});
+
+
+
+
