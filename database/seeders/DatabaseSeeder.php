344c5d1607a08db196ce956b3b5f0040cb5b14d6
Griswalde <agunggriswald158@gmail.com>
2021-10-21 12:23:10 +0700

    video bagian 3, #2 Factory & Faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,21 +16,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        
+        // User::create([
+        //     'name' =>'Agung Gumelar',
+        //     'email' => 'Agunggriswald158@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
 
-        User::create([
-            'name' =>'Agung Gumelar',
-            'email' => 'Agunggriswald158@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
 
+        // User::create([
+        //     'name' =>'wijdan',
+        //     'email' => 'wijdan@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
+        // User::factory(20)->create();
 
-        User::create([
-            'name' =>'wijdan',
-            'email' => 'wijdan@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
-
+        User::factory(3)->create();
 
        Category::create([
            'name' => 'programming',
@@ -42,73 +43,77 @@ class DatabaseSeeder extends Seeder
             'slug' =>'Personal'
         ]);
 
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug' => 'judul-pertama',
-            'excerpt' => 'I am the Bone of my Sword',
-            'body' => 'I am the Bone of my Sword
-            Steel is my Body and Fire is my Blood.
-            I have created over a Thousand Blades,
-            Unknown to Death,
-            Nor known to Life.
-            Have withstood Pain to create many Weapons
-            Yet those Hands will never hold Anything.
-            So, as I Pray',
-            'category_id' => 1,
-            'user_id' =>1
+        Post::factory(20)->create();
 
-        ]);
         
-        Post::create([
-            'title' => 'Judul ke Dua',
-            'slug' => 'judul-ke-dua',
-            'excerpt' => 'I am the Bone of my Sword',
-            'body' => 'I am the Bone of my Sword
-            Steel is my Body and Fire is my Blood.
-            I have created over a Thousand Blades,
-            Unknown to Death,
-            Nor known to Life.
-            Have withstood Pain to create many Weapons
-            Yet those Hands will never hold Anything.
-            So, as I Pray',
-            'category_id' => 1,
-            'user_id' =>1
 
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'I am the Bone of my Sword',
+        //     'body' => 'I am the Bone of my Sword
+        //     Steel is my Body and Fire is my Blood.
+        //     I have created over a Thousand Blades,
+        //     Unknown to Death,
+        //     Nor known to Life.
+        //     Have withstood Pain to create many Weapons
+        //     Yet those Hands will never hold Anything.
+        //     So, as I Pray',
+        //     'category_id' => 1,
+        //     'user_id' =>1
 
-        Post::create([
-            'title' => 'Judul ke tiga',
-            'slug' => 'judul-ke-tiga',
-            'excerpt' => 'I am the Bone of my Sword',
-            'body' => 'I am the Bone of my Sword
-            Steel is my Body and Fire is my Blood.
-            I have created over a Thousand Blades,
-            Unknown to Death,
-            Nor known to Life.
-            Have withstood Pain to create many Weapons
-            Yet those Hands will never hold Anything.
-            So, as I Pray',
-            'category_id' => 2,
-            'user_id' =>1
+        // ]);
+        
+        // Post::create([
+        //     'title' => 'Judul ke Dua',
+        //     'slug' => 'judul-ke-dua',
+        //     'excerpt' => 'I am the Bone of my Sword',
+        //     'body' => 'I am the Bone of my Sword
+        //     Steel is my Body and Fire is my Blood.
+        //     I have created over a Thousand Blades,
+        //     Unknown to Death,
+        //     Nor known to Life.
+        //     Have withstood Pain to create many Weapons
+        //     Yet those Hands will never hold Anything.
+        //     So, as I Pray',
+        //     'category_id' => 1,
+        //     'user_id' =>1
 
-        ]);
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul ke tiga',
+        //     'slug' => 'judul-ke-tiga',
+        //     'excerpt' => 'I am the Bone of my Sword',
+        //     'body' => 'I am the Bone of my Sword
+        //     Steel is my Body and Fire is my Blood.
+        //     I have created over a Thousand Blades,
+        //     Unknown to Death,
+        //     Nor known to Life.
+        //     Have withstood Pain to create many Weapons
+        //     Yet those Hands will never hold Anything.
+        //     So, as I Pray',
+        //     'category_id' => 2,
+        //     'user_id' =>1
+
+        // ]);
 
 
-        Post::create([
-            'title' => 'Judul ke Empat',
-            'slug' => 'judul-ke-empat',
-            'excerpt' => 'I am the Bone of my Sword',
-            'body' => 'I am the Bone of my Sword
-            Steel is my Body and Fire is my Blood.
-            I have created over a Thousand Blades,
-            Unknown to Death,
-            Nor known to Life.
-            Have withstood Pain to create many Weapons
-            Yet those Hands will never hold Anything.
-            So, as I Pray',
-            'category_id' => 2,
-            'user_id' =>2
+        // Post::create([
+        //     'title' => 'Judul ke Empat',
+        //     'slug' => 'judul-ke-empat',
+        //     'excerpt' => 'I am the Bone of my Sword',
+        //     'body' => 'I am the Bone of my Sword
+        //     Steel is my Body and Fire is my Blood.
+        //     I have created over a Thousand Blades,
+        //     Unknown to Death,
+        //     Nor known to Life.
+        //     Have withstood Pain to create many Weapons
+        //     Yet those Hands will never hold Anything.
+        //     So, as I Pray',
+        //     'category_id' => 2,
+        //     'user_id' =>2
 
-        ]);
+        // ]);
     }
 }
