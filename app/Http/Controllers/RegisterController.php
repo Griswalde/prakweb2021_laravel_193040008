603330cf5f9df327b7226d7a983712b79e07640a
Griswalde <agunggriswald158@gmail.com>
2021-10-28 14:34:07 +0700

    video bagian 4, #3 User Registration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -12,5 +13,24 @@ class RegisterController extends Controller
             'title' => 'Register',
             'active' => 'register'
         ]);
+    }
+
+    public function store(Request $request)
+    {
+       $validateData =  $request->validate([
+            'name' => 'required|max:255',
+            'username' =>['required','min:3', 'max:255','unique:users'],
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:5|max:255'
+        ]);
+
+        // $validateData['password'] = bcrypt($validateData['password']);
+        $validateDatap['password'] = Hash::make($validateData['password']);
+
+        User::create($validateData);
+        
+        // $request->session()->flash('success','Registration successsfull! Please login');
+
+        return redirect('/login')->with('success','Registration successsfull! Please login');
     }
 }
