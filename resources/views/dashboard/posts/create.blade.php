a37e682f01680ab779e065efc74eae3f59c8ad82
Griswalde <agunggriswald158@gmail.com>
2021-11-19 01:14:23 +0700

    video bagian 5, #3 Validation & Insert Post

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -5,30 +5,53 @@
     <h1 class="h2">Create new Post</h1>
 </div>
 <div class="col-lg-8">
-    <form method="post" action="/dashboard/posts">
+    <form method="post" action="/dashboard/posts" class="mb-5">
         @csrf
+      {{-- title --}}
 <div class="mb-3">
   <label for="title" class="form-label">Title</label>
-  <input type="text" class="form-control" id="title" name="title">
+  <input type="text" class="form-control @error('title')
+   is-invalid @enderror" id="title" 
+   name="title" required autofocus velue="{{ old('title') }}">
+   @error('title')
+    <div class="invalid-feedback">
+      {{ $message }}
+    </div>
+   @enderror
 </div>
-
+{{-- slug --}}
 <div class="mb-3">
   <label for="slug" class="form-label">Slug</label>
-  <input type="text" class="form-control" id="slug" name="slug" disabled readonly>
+  <input type="text" class="form-control
+  @error('slug')
+   is-invalid @enderror" id="slug" name="slug" required velue="{{ old('title') }}">>
+   @error('slug')
+    <div class="invalid-feedback">
+      {{ $message }}
+    </div>
+   @enderror
 </div>
+{{-- category --}}
 <div class="mb-3">
   <label for="category" class="form-label">category</label>
   <select class="form-select" name="category_id">
     @foreach ($categories as $category)
-    <option value="{{ $category->id }}">{{ $category->name }}</option>
-        
-    @endforeach
+      @if(old('category_id') == $category->id )
+              <option value="{{ $category->id }}" selected>{{ $category->name }}</option>
+              @else
+              (old('category_id') === $category_id )
+              <option value="{{ $category->id }}">{{ $category->name }}</option>
+              @endif
+              @endforeach
   </select>
 </div>
 
 <div class="mb-3">
   <label for="body" class="form-label">Body</label>
-  <input id="body" type="hidden" name="body">
+  @error('body')
+  <p class="text-danger">{{ $message }}</p>
+  @enderror
+  <input id="body" type="hidden" name="body" value="{{ old('body') }}"">
   <trix-editor input="body"></trix-editor>
 </div>
 
